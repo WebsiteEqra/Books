0d2a139cdee455b08241858ac974727e4ec1cc09
WebsiteEqra <eqrabooks@gmail.com>
2025-09-29 20:49:21 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+/* =============================
+   🌙☀️ الوضع الليلي / الفاتح
+============================= */
+const toggleBtn = document.getElementById("toggleMode");
+const body = document.body;
+
+// تحميل الوضع المحفوظ
+let savedMode = localStorage.getItem("mode");
+if (savedMode) {
+  body.classList.add(savedMode);
+  toggleBtn.textContent =
+    savedMode === "dark" ? "☀️ الوضع الفاتح" : "🌙 الوضع الداكن";
+} else {
+  body.classList.add("dark"); // الافتراضي داكن
+  toggleBtn.textContent = "☀️ الوضع الفاتح";
+}
+
+// عند الضغط على الزر
+toggleBtn.addEventListener("click", () => {
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+    body.classList.add("light");
+    localStorage.setItem("mode", "light");
+    toggleBtn.textContent = "🌙 الوضع الداكن";
+  } else {
+    body.classList.remove("light");
+    body.classList.add("dark");
+    localStorage.setItem("mode", "dark");
+    toggleBtn.textContent = "☀️ الوضع الفاتح";
+  }
+});
+
